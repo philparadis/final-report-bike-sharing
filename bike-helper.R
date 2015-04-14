a615c86d3b7495cf6b74b7200abe265e55157011
Philippe Paradis <philippe.paradis@carleton.ca>
2015-04-14 19:34:59 -0400

    Helper code for plotting.

---
+++ bike-helper.R
@@ -0,0 +1,48 @@
+# DESCRIPTION:
+# This file defines some helper functions related to the Capital Bike Sharing
+# dataset, such as plotting "cnt" graphs and compute error measures.
+
+source("bike-imputate.R")
+
+
+# Define some helper functions to calculate errors
+# Compute RMSE
+compute.rmse <- function(pred, actual)
+{
+  sqrt(mean((pred-actual)^2, na.rm=TRUE))
+}
+# Compute RMSLE
+compute.rmsle <- function(pred, actual)
+{
+  sqrt(mean((log(pred+1)-log(actual+1))^2, na.rm=TRUE))
+}
+
+# Plot "cnt" for a range of days
+bike.plot <- function(index.begin, index.end, data, feature="cnt", add=FALSE, ...)
+{
+  begin <- index.begin
+  end <- index.end
+  indices <- begin:end
+  if (isTRUE(add))
+    points(indices, data[indices, feature], type="o", ...)
+  else {
+    plot(indices, data[indices, feature], type="o", xaxt="n",
+        main=paste0("Plotting ", data[begin,"date"], " (",
+                    data[begin,"weekday"], ") to ", data[end,"date"], " (",
+                    data[end, "weekday"], ")"),
+        ...)
+    axis(1, at=seq(begin, end, 24), labels=FALSE)
+    axis(1, at=seq(begin+12, end, 24), tick=FALSE,
+         labels=paste0(data[seq(begin,end,24), "weekday"],
+                       " h=", data[seq(begin,end,24), "holiday"],
+                       "\n", data[seq(begin,end,24),"date"]))
+  }
+}
+
+bike.plot.dates <- function(date.begin, date.end, data, feature="cnt", add=FALSE, ...)
+{
+  begin <- which(data$date == as.POSIXct(date.begin, tz="UTC"))[[1]]
+  end <- which(data$date == as.POSIXct(date.end, tz="UTC"))[[1]]
+  end <- end + 23
+  bike.plot(begin, end, data=data, feature=feature, add=add, ...)
+}
