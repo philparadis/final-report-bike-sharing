73252d426aa102c567fda19fa3b14f45150d902e
Yue Dong <yuedongle@gmail.com>
2015-04-16 03:54:15 -0400

    Yue merge 1.

--- bike-cluster.R
+++ bike-cluster.R
@@ -1,4 +1,97 @@
-source("bike-imputate.R")
+source("bike-load.R")
+
+library(zoo)
+library(cluster)
+library(fpc)
+
+tmp <- bike.hourly[1:48,]
+plot(cnt ~ datetime, data=tmp, type="o")
+
+####################
+#build the matrix, each row is a day, 
+#24 attributes with counts from each hour in this day
+first.day <- bike.hourly$datetime[1]
+
+difftime(bike.hourly$date, first.day, units="days")+1
+
+
+# create a new data.frame that will contain 24 columns indicating the "cnt"
+# each row indicate a day
+bike.24hourscnt <- c()
+
+for (date in bike.daily$date) {
+  # Find all "cnt" values for the matching "date"
+  # Note: There might be less than 24 values since some hours are missing
+  results <- bike.hourly[bike.hourly[,2] == date, c("hr","cnt")]
+  # Here we are using some R black magic to handle NA values when
+  # constructing the row of "cnt" over 24 hours
+  counts <- rep(NA, 24)
+  counts[match(results[,1],0:23)] <- 0
+  counts[!is.na(counts)] <- results[,2]
+  names(counts) <- paste0("hr",0:23)
+  bike.24hourscnt <- rbind(bike.24hourscnt, data.frame(as.list(counts)))
+}
+rownames(bike.24hourscnt) <- 1:nrow(bike.daily)
+
+# Count the number of NA values per day
+apply(is.na(bike.24hourscnt), 1, sum)
+
+#inspect how many missing values in each row
+apply(is.na(bike.24hourscnt), 1, sum)
+
+# Count how many days have more than 1 NA value
+sum(apply(is.na(bike.24hourscnt), 1, sum) > 1)
+#head(bike.24hourscnt)
+#bike.24hourscnt[2,]
+
+#filling in the missing values by "zoo" package
+#identify the rows with 1 missing value
+#convert the data frame into a matrix
+M <- as.matrix(bike.24hourscnt)
+
+#filling method 1: fill the missing value by last observation 
+apply(is.na(bike.24hourscnt), 1, sum) == 1
+#filling second row as an example
+#found second row has 1 missing value, use as an example
+#plot show missing value at hr5
+M[2,]
+plot(1:24, M[2,],type='o')
+(na.locf(M[2,]))
+plot(1:24, na.locf(M[2,]),type='o')
+#fill the whole matrix
+# A <- matrix(unname(unlist(apply(M, 1, na.locf, na.rm=FALSE))), 731, 24)
+# B <- matrix(unname(unlist(apply(A, 1, na.locf, na.rm=FALSE, fromLast=TRUE))), 731, 24)
+# sum(apply(is.na(A), 1, sum) >0)
+# sum(apply(is.na(B), 1, sum) >0)
+
+A <- t(na.locf(t(M)))
+B <- t(na.locf(t(A), fromLast=TRUE))
+
+# #filling method 2:uses linear interpolation to fill in missing data
+# M[2,]
+# (na.approx(M[2,]))
+# plot(1:24, na.approx(M[2,]),type='o')
+# 
+# C <- matrix(unname(unlist(apply(A, 1, na.approx, na.rm=FALSE))), 731, 24)
+# sum(apply(is.na(C), 1, sum) >0)
+# apply(is.na(C), 1, sum) >0
+# D <- matrix(unname(unlist(apply(C, 1, na.approx, na.rm=FALSE))), 731, 24)
+# apply(is.na(D), 1, sum) >0
+# 
+# #filling method 3: uses polynomial interpolation to fill in missing data
+# M[2,]
+# (na.spline(M[2,]))
+# E <- matrix(unname(unlist(apply(A, 1, na.spline, na.rm=FALSE))), 731, 24)
+# sum(apply(is.na(E), 1, sum) >0)
+# F <- matrix(unname(unlist(apply(E, 1, na.spline, na.rm=FALSE))), 731, 24)
+# sum(apply(is.na(F), 1, sum) >0)
+# 
+# #draw a graph with different filling methods
+# plot(na.locf(M[40,]),type='o', col="red") 
+# lines(na.approx(M[40,]),type='o', col="green")
+# lines(na.spline(M[40,]), type='o', col="blue")
+# lines(M[40,], type='o')
+
 
 print.cluster.working.days <- function (data, cluster.indices, num.clusters)
 {
@@ -7,21 +100,9 @@ print.cluster.working.days <- function (data, cluster.indices, num.clusters)
     work.days <- sum(cl.data[,"workingday"]==1)
     total.days <- nrow(cl.data)
     cat(paste0("Cluster #", i, "\n"))
-    cat(paste0("Working days: ", work.days, " (",signif(work.days/total.days*100, 3),"%)\n"))
-    cat(paste0("Total days: ", total.days, "\n"))
-  }
-}
-print.cluster.holiday <- function (data, cluster.indices, num.clusters)
-{
-  for (i in 1:num.clusters) {
-    cl.data <- data[cluster.indices==i,]
-    holiday.days <- sum(cl.data[,"holiday"]==1)
-    weekend.days <- sum(cl.data[,"weekday"]=="Sat" | cl.data[,"weekday"]=="Sun")
-    total.days <- nrow(cl.data)
-    cat(paste0("Cluster #", i, "\n"))
-    cat(paste0("Holiday days: ", holiday.days, " (",signif(holiday.days/total.days*100, 3),"%)\n"))
-    cat(paste0("Weekend days: ", weekend.days, " (",signif(weekend.days/total.days*100, 3),"%)\n"))
-    cat(paste0("Total days: ", total.days, "\n"))
+    cat(paste0("Working days: ", work.days, "\n"))
+    cat(paste0("total days: ", total.days, "\n"))
+    cat(paste0("percentage: ", work.days/total.days*100, "\n"))
   }
 }
 print.cluster.avg.temp <- function (data, cluster.indices, num.clusters)
@@ -31,39 +112,29 @@ print.cluster.avg.temp <- function (data, cluster.indices, num.clusters)
     avg.tmp <- mean(cl.data[,"atemp"])
     total.days <- nrow(cl.data)
     cat(paste0("Cluster #", i, "\n"))
-    cat(paste0("Avg temperature: ", signif(avg.tmp, 4), "\n"))
-    cat(paste0("Total days: ", total.days, "\n"))
+    cat(paste0("avg temperature: ", avg.tmp, "\n"))
+    cat(paste0("total days: ", total.days, "\n"))
   }
 }
 
 #applying k-means after dealing with missing values
 #kmeans on first filling: Last Observation Carried Forward
 num.clusters <- 2
-km1<-kmeans(M.approx, num.clusters, 300)
+km1<-kmeans(B, num.clusters, 300)
 print.cluster.working.days(bike.daily, km1$cluster, num.clusters)
 num.clusters <- 3
-km1<-kmeans(M.approx, num.clusters, 300)
+km1<-kmeans(B, num.clusters, 300)
 print.cluster.working.days(bike.daily, km1$cluster, num.clusters)
 num.clusters <- 4
-km1<-kmeans(M.approx, num.clusters, 300)
+km1<-kmeans(B, num.clusters, 300)
 print.cluster.working.days(bike.daily, km1$cluster, num.clusters)
 num.clusters <- 5
-km1<-kmeans(M.approx, num.clusters, 300)
+km1<-kmeans(B, num.clusters, 300)
 print.cluster.working.days(bike.daily, km1$cluster, num.clusters)
 num.clusters <- 6
-km1<-kmeans(M.approx, num.clusters, 300)
+km1<-kmeans(B, num.clusters, 300)
 print.cluster.working.days(bike.daily, km1$cluster, num.clusters)
 print.cluster.avg.temp(bike.daily, km1$cluster, num.clusters)
-
-num.clusters <- 4
-km1<-kmeans(M.approx, num.clusters, 300)
-print.cluster.working.days(bike.daily, km1$cluster, num.clusters)
-print.cluster.avg.temp(bike.daily, km1$cluster, num.clusters)
-print.cluster.holiday(bike.daily, km1$cluster, num.clusters)
-par(mfrow=c(2, 2));
-for (i in 1:4) {
-  plot(0:23, colMeans(M.approx[km1$cluster==i,]), main=paste0("Cluster ",i), type="o")
-}
 
 # plotcluster(B, km1$cluster, col=B.all$workingday)
 # clusplot(B, km1$cluster, color=TRUE, shade=TRUE, 
@@ -101,8 +172,8 @@ DBI <- rep(0, 10)
 #
 set.seed(0)
 for (i in 2:15) {
-  KM <- kmeans(M.approx, i, 15)
-  plot(M.approx, col = KM$cluster, pch = KM$cluster,
+  KM <- kmeans(B, i, 15)
+  plot(B, col = KM$cluster, pch = KM$cluster,
        main = paste(i," clusters"))
   errs[i-1] <- sum(KM$withinss)
   DBI[i-1] <- Davies.Bouldin(KM$centers, KM$withinss, KM$size)
--- bike-plot.R
+++ bike-plot.R
@@ -40,6 +40,9 @@ panel.hist <- function(x, ...) {
 pairs(bike.daily[,3:15], upper.panel=panel.cor, diag.panel=panel.hist)
 pairs(bike.daily[,3:15], col= bike.daily$season + 1)#, upper.panel=panel.cor, diag.panel=panel.hist)
 ##########################################################
+#plot temp vs. cnt
+plot(cnt~temp,data=bike.daily,  col=weathersit)
+
 # Histogram of rentals count (daily)
 hist(bike.daily$cnt)
 
@@ -54,12 +57,25 @@ plot(cnt ~ weathersit, data=bike.hourly)
 coplot(casual ~ cnt|weathersit, data=bike.hourly)
 
 #when it is weekend or holiday, less registered users
-coplot(registered ~ cnt|weathersit,  data=bike.hourly)
-coplot(registered ~ cnt|weathersit, col=as.integer(bike.hourly$workingday), data=bike.hourly)
+#coplot(registered ~ cnt|weathersit,  data=bike.hourly)
+coplot(registered ~ cnt|season, col=as.integer(bike.hourly$workingday), data=bike.hourly)
+legend( x="topleft", 
+        legend=c("workingday=1","workingday=0" ),
+        col=c(2,1), 
+        pch=c(1,1) )
+coplot(casual ~ cnt|season, col=as.integer(bike.hourly$workingday), data=bike.hourly)
+legend( x="topleft", 
+        legend=c("workingday=1","workingday=0" ),
+        col=c(2,1), 
+        pch=c(1,1) )
+#coplot(casual ~ registered|season,col=as.numeric(bike.daily$season)+1, data=bike.daily)
+coplot(registered ~ casual|mnth, col=as.integer(bike.hourly$workingday), data=bike.hourly)
+legend( x="bottomleft", 
+        legend=c("workingday=1","workingday=0" ),
+        col=c(2,1), 
+        pch=c(1,1), bty = "n",cex=0.7)
 
-coplot(casual ~ registered|cnt, data=bike.daily)
-coplot(casual ~ registered|mnth,col=bike.daily$season+1, data=bike.daily)
-coplot(casual ~ registered|season,col=as.numeric(bike.daily$season)+1, data=bike.daily)
+
 # histograms
 
 avg.by.season <- aggregate(.~season, bike.daily, mean)
@@ -71,7 +87,7 @@ boxplot(50*atemp ~ mnth, data=bike.daily)
 boxplot(temp ~ mnth, data=bike.daily)
 boxplot(hum ~ mnth, data=bike.daily)
 boxplot(windspeed~ mnth, data=bike.daily)
-boxplot(casual~ mnth, data=bike.daily)
+boxplot(casual~ mnth, data=bike.daily, ylab="casual counts")
 boxplot(registered~ mnth, data=bike.daily)
 
 #################################################################################
@@ -102,9 +118,29 @@ hist(bike.hourly$cnt)
 plot(cnt ~ weathersit, data=bike.daily)
 plot(cnt ~ weathersit, data=bike.hourly)
 
+#Explore 1: temp vs. count colored on weather
+plot(cnt ~ temp, data=bike.daily, col=as.numeric(bike.daily$weathersit)+1,
+     pch=as.numeric(bike.daily$weathersit)+5, main="Temperature vs. Counts" )
+legend( x="topleft", 
+        legend=c("weathersit=clear","weathersit=misty", "weathersit=rainy", "weathersit=stormy" ),
+        col=c(2,3,4,5), 
+        pch=c(6,7,8,9) )
+
+#Explore 2: hour vs. count colored on workingday
+plot(cnt ~ hr, data=bike.hourly, col=as.numeric(bike.hourly$workingday)+1,
+     pch=as.numeric(bike.daily$workingday)+5, main="Hour vs. Counts" )
+lines(cnt ~ hr, aggregate(.~hr, data=bike.hourly[which (bike.hourly$workingday==1),], mean), type="o", lwd=3,col="dark green")
+lines(cnt ~ hr, aggregate(.~hr, data=bike.hourly[which (bike.hourly$workingday==0),], mean), type="o",lwd=3, col="dark red")
+legend( x="topleft", 
+        legend=c("workingday=1","workingday=0", "mean (workingday=1)", "mean (workingday=0)" ),
+        col=c(2,3,"dark green","dark red"), lwd=3, lty=c(NA,NA,1,1),
+        pch=c(6,7,NA,NA), cex=0.8)
+
+
+
 # plot hourly counts conditional on day of the week
 hourly.avg.by.hr.and.weekday <- aggregate(.~hr+weekday, data=bike.hourly, mean)
-xyplot(cnt ~ hr | weekday, data=hourly.avg.by.hr.and.weekday, type="o")
+xyplot(cnt ~ hr | weekday, data=hourly.avg.by.hr.and.weekday, type="o", main="Average hourly bike counts by weekdays")
 
 # plot hourly counts conditional on weekday AND if it's a holiday
 specialdays <- aggregate(.~hr+weekday+holiday, data=bike.hourly, mean)
@@ -113,43 +149,49 @@ xyplot(cnt ~ hr | weekday+holiday, data=specialdays, type="o")
 # histograms
 
 # plot of average daily count for bike rental
+aggr.by.hour<-aggregate(.~hr, data=bike.hourly, mean)
 plot(cnt ~ hr, data=aggr.by.hour, type="o")
 
+
 # xyplot of average hourly count conditional on month of the year
-xyplot(cnt ~ hr | mnth, data=hourly.avg.by.hour.and.mnth)
+hourly.avg.by.hour.and.mnth<-aggregate(.~hr+mnth, data=bike.hourly, mean)
+xyplot(cnt ~ hr | mnth, data=hourly.avg.by.hour.and.mnth,type='o')
 
 # coplot of average daily count for bike rental conditionally on season
+hourly.avg.by.hour.and.season<- aggregate(.~hr+season, data=bike.hourly, mean)
 coplot(cnt ~ hr | season, data=hourly.avg.by.hour.and.season, type="o")
 
 # coplot of average daily count for bike rental conditionally on season
 coplot(cnt ~ hr | mnth, data=hourly.avg.by.hour.and.mnth, type="o")
 
 # barplot average daily count for bike rental
-xlabs <- barplot(hourly.avg.by.hour$cnt)
-axis(side=1, at=xlabs, labels=as.character(hourly.avg.by.hour$hr))
+xlabs <- barplot(aggr.by.hour$cnt)
+axis(side=1, at=xlabs, labels=as.character(aggr.by.hour$hr))
+               
+# boxplots
+boxplot(50*atemp ~ mnth, data=bike.daily)
+boxplot(cnt ~ mnth, data=bike.daily, Main="Boxplot of counts by Month")
 
-# more barplot
-barplot(table(daily.avg.by.season[,c("season","temp","hum","casual","registered")])
-        barplot(table(cars[,c("cylinders", "origin")]), mar=c(0,0,0,0),
-                beside=TRUE, col=rainbow(5), ylab= "Total",
-                legend.text=c("3 cylinders", "4 cylinders", "5 cylinders",
-                              "6 cylinders", "8 cylinders"),
-                args.legend=list(x=18.6, y=155, cex=0.85, ncol=3))        
-        
-        
-        # boxplots
-        boxplot(50*atemp ~ mnth, data=bike.daily)
-        
-        # Plot typical week
-        plot(cnt ~ datetime, data=bike.hourly[1:(24*7),], type="o")
-        
-        # Plot typical day
-        plot(cnt ~ hr, aggregate(.~hr, data=bike.hourly, mean), type="o", col="green")
-        points(cnt ~ hr, data=bike.hourly[(1:24)+24*100+2,], type="o")
-        
-        
-        # TODO: How can we easily visualize where the PEAK rental periods are? Not just
-        # on a weekly basis, but overall.
+# Plot typical week
+plot(cnt ~ datetime, data=bike.hourly[1:(24*7),], type="o")
+
+# Plot typical day
+plot(cnt ~ hr, aggregate(.~hr, data=bike.hourly, mean), type="o", col="green")
+points(cnt ~ hr, data=bike.hourly[(1:24)+24*100+2,], type="o")
+
+# boxplots
+boxplot(50*atemp ~ mnth, data=bike.daily)
+
+# More plots of atemp
+#plot(cnt ~ atemp, data=bike.hourly)
+hourly.avg.by.atemp <- aggregate(cnt~atemp, data=bike.hourly, mean)
+plot(cnt ~ atemp, data=hourly.avg.by.atemp)
+plot(cnt ~ abs(atemp-0.7), data=hourly.avg.by.atemp)
+
+hourly.avg.by.temp <- aggregate(cnt~temp, data=bike.hourly, mean)
+plot(cnt ~ temp, data=hourly.avg.by.temp)
+plot(cnt ~ abs(temp-0.7), data=hourly.avg.by.temp)
+
         
         
 
